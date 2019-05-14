3b89b10640e65e20bab55ea80c3d48ea408fe91b
Farah <41633573+farahriz@users.noreply.github.com>
2019-05-14 11:01:38 +0800

    Fix import issue

---
+++ src/components/__tests__/Item.spec.js
@@ -0,0 +1,11 @@
+import Item from '../Item.vue'
+
+describe('Item.vue', () => {
+    test('renders "item"', () => {
+        console.log(Item)
+        // const Ctor = Vue.extend(Item)
+        // const vm = new Ctor().$mount()
+        // expect (vm.$el.textContent).toContain('item')
+    })
+})
+
