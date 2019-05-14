067f0460c980a2efb316b8e17669a85b25d236b1
Farah <41633573+farahriz@users.noreply.github.com>
2019-05-14 12:03:03 +0800

    Finally wrote that first unit test

--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -1,11 +1,11 @@
+import Vue from 'vue'
 import Item from '../Item.vue'
 
 describe('Item.vue', () => {
     test('renders "item"', () => {
-        console.log(Item)
-        // const Ctor = Vue.extend(Item)
-        // const vm = new Ctor().$mount()
-        // expect (vm.$el.textContent).toContain('item')
+        const Ctor = Vue.extend(Item)
+        const vm = new Ctor().$mount()
+        expect (vm.$el.textContent).toContain('item')
     })
 })
 
