af3cc4e612c38502f0097a2a86e378b2eb00f695
Farah <41633573+farahriz@users.noreply.github.com>
2019-05-14 14:52:10 +0800

    Add in link and test for link

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,8 +1,16 @@
 <template>
-  <li>
-    {{item.url}}
-    {{item.score}}
-    Written by {{item.by}}
+  <li class="item">
+    <span class="score">{{ item.score }}</span>
+    <span class="title">
+      <a :href="item.url" target="_blank" rel="noopener">{{ item.title }}</a>
+      <span class="host"> ({{ item.url }})</span>
+    </span>
+    <br>
+    <span class="meta">
+      <span class="by">
+        by {{ item.by }}
+      </span>
+    </span>
   </li>
 </template>
 
--- src/components/__tests__/Item.spec.js
+++ src/components/__tests__/Item.spec.js
@@ -22,4 +22,26 @@ describe('Item.vue', () => {
     expect(wrapper.text()).toContain(item.score)
   })
 
+  test('renders item.by', () => {
+      const item = {
+        by: "Marcus Aurelius" 
+      }
+      const wrapper = shallowMount(Item, {
+        propsData: { item }
+      })
+      expect(wrapper.text()).toContain(item.by)
+  })
+
+  test('renders a link to the item.url with item.title as text', () => {
+      const item = {
+          url: "http://www.somewhere.url",
+          title: "Lorem Ipsum"
+      }
+      const wrapper = shallowMount(Item, {
+        propsData: {item}
+      })
+      const a = wrapper.find('a')
+      expect(a.text()).toBe(item.title)
+      expect(a.attributes().href).toBe(item.url)
+  })
 })
