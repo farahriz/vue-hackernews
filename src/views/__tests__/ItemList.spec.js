8cfcf2a8d5b796c74dbe57061fda5ab941a736a7
Farah <41633573+farahriz@users.noreply.github.com>
2019-05-14 15:09:40 +0800

    ItemList test to check if items are rendering

--- src/views/__tests__/ItemList.spec.js
+++ src/views/__tests__/ItemList.spec.js
@@ -2,11 +2,15 @@ import { shallowMount } from '@vue/test-utils'
 import ItemList from '../ItemList.vue'
 import Item from '../../components/Item.vue'
 
-describe('ItemList.vue', () = {
+describe('ItemList.vue', () => {
     test('renders an Item with data for each item in windows.items', () => {
         window.items = [{}, {}, {}]
         const wrapper = shallowMount(ItemList)
-        // const items = wrapper.findAll(Item)
+        const items = wrapper.findAll(Item)
         expect(wrapper.findAll(Item)).toHaveLength(window.items.length)
+        items.wrappers.forEach((wrapper, i) => {
+            expect(wrapper.props().item).toBe(winodw.items[i])
+        })
     })
+
 })
